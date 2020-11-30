4c2eb4505feffc172bb25b03708149842b3a1fc2
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-30 22:37:16 +0900

    onloadの定義

--- app/javascript/packs/checked.js
+++ app/javascript/packs/checked.js
@@ -7,6 +7,14 @@ function check() {
       XHR.open("GET", `/posts/${postId}`, true);
       XHR.responseType = "json";
       XHR.send();
+      XHR.onload = () => {
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+      };
     });
   });
 }
