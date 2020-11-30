141c0dd3110b807e09f64f0327295359171273a7
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-30 22:50:14 +0900

    jsファイルの移動

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
+
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+  
   def checked
     post = Post.find(params[:id])
     if post.checked
@@ -13,8 +15,8 @@ class PostsController < ApplicationController
     else
       post.update(checked: true)
     end
+
     item = Post.find(params[:id])
     render json: { post: item }
   end
-
 end
